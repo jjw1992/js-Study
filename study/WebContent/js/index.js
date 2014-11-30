95133b3813879f84e8b28fe0e1429aed2fe88108
jinwoo <jinwoo@jinwoo-PC>
2014-11-30 12:36:50 +0900

    123

---
+++ study/WebContent/js/index.js
@@ -0,0 +1,25 @@
+
+var jsStudy = jsStudy ||{};
+jsStudy.join ={};
+jsStudy.join.test = function(){
+	alert("test");
+};
+/*jsStudy.checkPasswd = function(pw){};
+jsStudy.checkId = function(id){
+	document.getElementById("id");alert("아이디");
+	document.getElementById("");alert("아이디를 입력하세요!");
+	jsStudy.submit();*/
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
